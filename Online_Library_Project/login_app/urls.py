f5146460534d41bacfecec87017c6f314c55b403
shreyaDeb <shreya.deb@icloud.com>
2023-10-10 00:51:34 -0600

    semi final

--- Online_Library_Project/login_app/urls.py
+++ Online_Library_Project/login_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path("login/", views.user_login, name="user_login"),
     # Add more URLs for login-related views as needed
